abe1f6b4040f40a8efb49f4a3343536943dbf3c9
KoruzhynetsAnna <121615437+KoruzhynetsAnna@users.noreply.github.com>
2023-06-27 17:17:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+var age = 13;
+
+console.log(age);
+
+
+var name = "Anya";
+
+console.log(name);
+
+
+var isStudent = true;
+
+console.log(isStudent);
+
+
+var myString = "Life is what happens when you're busy making other plans.";
+
+console.log(myString);
+
+
+var myNumber = 5;
+
+myNumber += 10;
+
+console.log(myNumber);
+
+var myNull = null;
+
+console.log(myNull);
+
+
+var userName = prompt("Введіть ваше ім'я:");
+
+alert("Привіт, " + userName + "!");
+
+
+var confirmation = confirm("Ви погоджуєтесь з цією дією?");
+
+if (confirmation) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+
+
+alert("Ця дія небезпечна!");
+
+var dangerConfirmation = confirm("Ви погоджуєтесь з цією дією?");
+
+if (dangerConfirmation) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
